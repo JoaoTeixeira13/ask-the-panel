4c6d1adade6e7afa80e0912033a34774a3dae951
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2025-05-07 22:21:48 +0200

    add question length validation to zod schema

--- src/types/panel.ts
+++ src/types/panel.ts
@@ -5,7 +5,10 @@ import { ResponseLength } from "@/types/responseLength";
 export const PanelRequestSchema = z.object({
     selectedCharacter: z.nativeEnum(CharacterId),
     responseLength: z.nativeEnum(ResponseLength),
-    question: z.string().min(1),
+    question: z
+        .string()
+        .min(10, "Question must be at least 10 characters long.")
+        .max(160, "Question must be no more than 160 characters."),
 });
 
 export type PanelRequest = z.infer<typeof PanelRequestSchema>;
