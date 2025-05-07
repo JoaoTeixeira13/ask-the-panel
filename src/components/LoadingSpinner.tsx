1a8ff0da6218ba50325e967a125c681ea9a332ec
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2025-05-07 21:31:56 +0200

    replace img with nextJs Image element on loading spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 type LoadingSpinnerProps = {
     loading: boolean;
@@ -8,10 +9,12 @@ export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ loading }) => {
     return (
         <div className="h-12 flex justify-center items-center mt-3 mb-3">
             {loading && (
-                <img
-                    src="images/coral.png"
+                <Image
+                    src="/images/coral.png"
                     alt="Loading..."
-                    className="h-12 w-12 animate-spin rounded-full border border-dotted border-accent"
+                    width={48}
+                    height={48}
+                    className="animate-spin rounded-full border border-dotted border-accent"
                 />
             )}
         </div>
